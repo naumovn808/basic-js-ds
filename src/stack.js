cdb47bbb52a4ba06a8c0e3fc88278e4bf859cebc
Nikita Naumov <naumovn808@gmail.com>
2023-04-13 10:02:28 +0500

    feat: task stack is done

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.arr = [];
+  }
+
+  push(elem) {
+    this.arr.push(elem);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      return this.arr.pop(this.arr[- 1]);
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr[this.arr.length - 1];
   }
 }
 
